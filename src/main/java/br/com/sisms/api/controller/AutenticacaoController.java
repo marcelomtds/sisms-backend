a1234304ff960da933dacbc1f5819f7f30e6d275
Marcelo <marcelo.tadeu@castgroup.com.br>
2020-01-31 12:14:47 -0300

    Ajusta cross origin

--- src/main/java/br/com/sisms/api/controller/AutenticacaoController.java
+++ src/main/java/br/com/sisms/api/controller/AutenticacaoController.java
@@ -10,16 +10,14 @@ import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Api(tags = "Autenticação")
 @RestController
 @AllArgsConstructor
+@CrossOrigin("**")
 @RequestMapping(path = "/api/auth", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AutenticacaoController {
 
